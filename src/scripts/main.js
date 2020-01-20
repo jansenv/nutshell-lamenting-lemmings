0f3472b095fc82121f64def8afde3ce970a0fcbe
jansenv <jansenv44@gmail.com>
2020-01-20 14:09:07 -0600

    deleted ads

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,6 @@ import { getMessages } from "./messages/MessagesDataProvider.js";
 import { getFriends } from "./friends/FriendsDataProvider.js";
 import { addFriend } from "./friends/AddOrRemoveFriend.js";
 import { getNews } from "./news/NewsDataProvider.js";
-<<<<<<< HEAD
-import NewsList from "./news/NewsList.js";
-import { AddNewsForm } from "./news/NewsForm.js";
-import { ads } from "./news/ads.js";
-=======
 import { AddEventForm } from "./events/EventForm.js"
 import EventList from "./events/EventList.js"
 import { AddTaskForm } from "./tasks/TaskForm.js"
@@ -21,7 +16,6 @@ import TaskList from "./tasks/TaskList.js"
 import NewsList from "./news/NewsList.js"
 import { FriendList } from "./friends/FriendList.js"
 import { Home } from "./homeNav/home.js";
->>>>>>> master
 
 
 // Renders all html for user
@@ -50,11 +44,3 @@ getUsers()
 .then(RenderAtLogin)
 .then(Home)
 
-<<<<<<< HEAD
-getNews()
-.then(AddNewsForm)
-.then(NewsList)
-
-ads()
-=======
->>>>>>> master
